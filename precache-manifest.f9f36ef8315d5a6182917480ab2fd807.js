2dce37610dd0955683f3ec6cac859df786e1c8f5
little_buddha <somineegupta@gmail.com>
2020-04-26 12:28:34 +0530

    Updates

---
+++ precache-manifest.f9f36ef8315d5a6182917480ab2fd807.js
@@ -0,0 +1,178 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ffb169074861b0a4455e93966d230dc9",
+    "url": "/AGA-Project/index.html"
+  },
+  {
+    "revision": "efce91895ee02d38e11a",
+    "url": "/AGA-Project/static/css/main.80a423e0.chunk.css"
+  },
+  {
+    "revision": "ceecd2c8884874958a61",
+    "url": "/AGA-Project/static/js/2.3a383def.chunk.js"
+  },
+  {
+    "revision": "7eac5df8fd17c53ea42b25fbf2237fb3",
+    "url": "/AGA-Project/static/js/2.3a383def.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "efce91895ee02d38e11a",
+    "url": "/AGA-Project/static/js/main.b20499f3.chunk.js"
+  },
+  {
+    "revision": "c5da3395596a2b8aa306",
+    "url": "/AGA-Project/static/js/runtime-main.cec6935e.js"
+  },
+  {
+    "revision": "0492ac6c40632753621e65d76619c1a8",
+    "url": "/AGA-Project/static/media/Airport.0492ac6c.svg"
+  },
+  {
+    "revision": "d93c7db3f2d0bfee3d69bbb99499879d",
+    "url": "/AGA-Project/static/media/Airport.d93c7db3.png"
+  },
+  {
+    "revision": "56a58f1d88109eaecb1aec8ba09998cf",
+    "url": "/AGA-Project/static/media/Airport_icon.56a58f1d.svg"
+  },
+  {
+    "revision": "279bfdbf4d043971f13af42c771cfd39",
+    "url": "/AGA-Project/static/media/Autsera.279bfdbf.png"
+  },
+  {
+    "revision": "b2e0bb8ff0d91316fb64223acab5f072",
+    "url": "/AGA-Project/static/media/AutseraHelpFinal.b2e0bb8f.mp4"
+  },
+  {
+    "revision": "35dc86f3d254cac803bce74b33eb3fb0",
+    "url": "/AGA-Project/static/media/Background_Home.35dc86f3.svg"
+  },
+  {
+    "revision": "c2ffe075932493fb94355859ddb0b111",
+    "url": "/AGA-Project/static/media/Blackboard.c2ffe075.svg"
+  },
+  {
+    "revision": "d31e3373609379367d7c5dc0c36d9443",
+    "url": "/AGA-Project/static/media/Blackboard.d31e3373.png"
+  },
+  {
+    "revision": "688a4e29527fe2fdbfe06ac59e5af7f0",
+    "url": "/AGA-Project/static/media/Boys_ball.688a4e29.svg"
+  },
+  {
+    "revision": "d3646b91e0146f942ebf371a5d2bebe3",
+    "url": "/AGA-Project/static/media/Boys_ball.d3646b91.png"
+  },
+  {
+    "revision": "2b974a9780051d91d20a2efbccc170da",
+    "url": "/AGA-Project/static/media/Bullying.2b974a97.svg"
+  },
+  {
+    "revision": "c6970f088e36101422121f0a3fab0979",
+    "url": "/AGA-Project/static/media/Bullying.c6970f08.png"
+  },
+  {
+    "revision": "3f373dc6fd8f378473d7c1c83ce7bf01",
+    "url": "/AGA-Project/static/media/Classroom.3f373dc6.svg"
+  },
+  {
+    "revision": "67d4f2b8f73c73e24d839450760f9ff3",
+    "url": "/AGA-Project/static/media/Classroom.67d4f2b8.png"
+  },
+  {
+    "revision": "edf31e0649f5f3744fdba271b3862817",
+    "url": "/AGA-Project/static/media/Fight_ball.edf31e06.png"
+  },
+  {
+    "revision": "fd3f51b2f990b8a45a0be0d74d4db94d",
+    "url": "/AGA-Project/static/media/Fight_ball.fd3f51b2.svg"
+  },
+  {
+    "revision": "842f35bc7e6daa4534d54695693806c8",
+    "url": "/AGA-Project/static/media/Hippo.842f35bc.svg"
+  },
+  {
+    "revision": "778c861333283406728c73c3fd491276",
+    "url": "/AGA-Project/static/media/Hugging.778c8613.svg"
+  },
+  {
+    "revision": "ab6cbe5b5b155c10503e198207d28780",
+    "url": "/AGA-Project/static/media/Hugging.ab6cbe5b.png"
+  },
+  {
+    "revision": "e8bf0a249802c5676ebea2adc40c2585",
+    "url": "/AGA-Project/static/media/Isolation.e8bf0a24.svg"
+  },
+  {
+    "revision": "eb47b67b7ce9f5500a98e919cab82236",
+    "url": "/AGA-Project/static/media/Isolation.eb47b67b.png"
+  },
+  {
+    "revision": "860dac29fdba553f4709381b48bf021d",
+    "url": "/AGA-Project/static/media/Playground.860dac29.svg"
+  },
+  {
+    "revision": "f0625d4b1711ad7d0b4e2944e2fb7882",
+    "url": "/AGA-Project/static/media/Playground.f0625d4b.png"
+  },
+  {
+    "revision": "017641a400da2f02d342bb2145d8b545",
+    "url": "/AGA-Project/static/media/Playground_icon.017641a4.svg"
+  },
+  {
+    "revision": "de1e370c7c86a534d5eb66451df0af50",
+    "url": "/AGA-Project/static/media/Running.de1e370c.svg"
+  },
+  {
+    "revision": "ea215c8da166deb81a32c85336e7c523",
+    "url": "/AGA-Project/static/media/Running.ea215c8d.png"
+  },
+  {
+    "revision": "02869c9ae13758ee43493a22d2afe0cc",
+    "url": "/AGA-Project/static/media/School.02869c9a.svg"
+  },
+  {
+    "revision": "1bfa3f29a0973908792b10cfa651b4e2",
+    "url": "/AGA-Project/static/media/School.1bfa3f29.png"
+  },
+  {
+    "revision": "f0c8fba00b47fc086381bca9df4472f2",
+    "url": "/AGA-Project/static/media/School_icon.f0c8fba0.svg"
+  },
+  {
+    "revision": "cef28f75237d4872ae2ee14fc6ad85bb",
+    "url": "/AGA-Project/static/media/Slide.cef28f75.png"
+  },
+  {
+    "revision": "ec84705901379582e0236c3e5e8c7b2c",
+    "url": "/AGA-Project/static/media/Slide.ec847059.svg"
+  },
+  {
+    "revision": "8f5e66be921ee511c1e91cad01f8be85",
+    "url": "/AGA-Project/static/media/Slide_happy.8f5e66be.svg"
+  },
+  {
+    "revision": "a69fec32ea17a29e2658a37516d09593",
+    "url": "/AGA-Project/static/media/Slide_happy.a69fec32.png"
+  },
+  {
+    "revision": "10499885060a829d9cb0b5807ec517cb",
+    "url": "/AGA-Project/static/media/Talking.10499885.svg"
+  },
+  {
+    "revision": "567facd9358b162887e9fa2acd1ba0d4",
+    "url": "/AGA-Project/static/media/Talking.567facd9.png"
+  },
+  {
+    "revision": "99f5ac092206a5c539e42ab2157b4c45",
+    "url": "/AGA-Project/static/media/Town.99f5ac09.svg"
+  },
+  {
+    "revision": "338595cb7f848ce2471430eca7d96d00",
+    "url": "/AGA-Project/static/media/lock.338595cb.svg"
+  },
+  {
+    "revision": "459fd8adb2ca9278fd8268de873a21e5",
+    "url": "/AGA-Project/static/media/map.459fd8ad.png"
+  }
+]);
